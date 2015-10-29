74576cb217f3fa01c61ce0b186ffd067363acc85
jr-dig-orgel <thomas.orgel@joanneum.at>
2015-10-29 14:03:16 +0100

    added calc histogram from value lengths

--- src/main/java/eu/eexcess/dataquality/structure/StructureRecResult.java
+++ src/main/java/eu/eexcess/dataquality/structure/StructureRecResult.java
@@ -8,8 +8,53 @@ public class StructureRecResult {
 	
 	protected int records=0;
 	
+	protected int lengthMax =-1;
+	
+	protected int lengthMaxTrimmed = -1;
+	
+	protected int lengthMin;
+	
+	protected int lengthMinTrimmed;
+
+	protected int[] lengthHistogram;
+	
+	public void initLengthHistogramm(int size) {
+		this.lengthHistogram = new int[size+1];
+		for (int i = 0; i < lengthHistogram.length; i++) {
+			lengthHistogram[i] = 0;
+		}
+	}
+	
+	public void addValueLengthHistogram(int length) {
+		lengthHistogram[length]++;
+	}
+	
 	public boolean isStructureDetected() {
 		return structureDetected;
+	}
+
+	public int getLengthMax() {
+		return lengthMax;
+	}
+
+	public void setLengthMax(int lengthMax) {
+		this.lengthMax = lengthMax;
+	}
+
+	public int getLengthMaxTrimmed() {
+		return lengthMaxTrimmed;
+	}
+
+	public void setLengthMaxTrimmed(int lengthMaxTrimmed) {
+		this.lengthMaxTrimmed = lengthMaxTrimmed;
+	}
+
+	public int[] getLengthHistogram() {
+		return lengthHistogram;
+	}
+
+	public void setLengthHistogram(int[] lengthHistogram) {
+		this.lengthHistogram = lengthHistogram;
 	}
 
 	public void setStructureDetected(boolean structureDetected) {
@@ -35,8 +80,37 @@ public class StructureRecResult {
 	public String toString()
 	{
 		String out = "records:" + records + "\n";
-		out += "structureDetected:" + structureDetected + "\n";
+		out += "lengthMax:" + lengthMax + "\n";
+		out += "lengthMaxTrimmed:" + lengthMaxTrimmed + "\n";
+		out += "lengthMin:" + lengthMin + "\n";
+		out += "lengthMinTrimmed:" + lengthMinTrimmed + "\n";
+		out += "lengthHistogram:\n";
+		for (int i = 0; i < lengthHistogram.length; i++) {
+			out += i + "\t";
+		}
+		out += "\n";
+		for (int i = 0; i < lengthHistogram.length; i++) {
+			out += lengthHistogram[i] + "\t";
+		}
+		out += "\n";
 		out += "pattern:" + pattern + "\n";
+		out += "structureDetected:" + structureDetected + "\n";
 		return out;
+	}
+
+	public int getLengthMin() {
+		return lengthMin;
+	}
+
+	public void setLengthMin(int lengthMin) {
+		this.lengthMin = lengthMin;
+	}
+
+	public int getLengthMinTrimmed() {
+		return lengthMinTrimmed;
+	}
+
+	public void setLengthMinTrimmed(int lengthMinTrimmed) {
+		this.lengthMinTrimmed = lengthMinTrimmed;
 	}
 }
--- src/main/java/eu/eexcess/dataquality/structure/StructureRecognizer.java
+++ src/main/java/eu/eexcess/dataquality/structure/StructureRecognizer.java
@@ -7,11 +7,44 @@ public class StructureRecognizer {
 	
 	public StructureRecResult analyse(List<String> values) {
 		StructureRecResult result = new StructureRecResult();
-		for (String string : values) {
+		result = calcLengths(values, result);
+		if (result.getRecords() == 0) {
+			return result;
+		}
+		result.initLengthHistogramm(result.getLengthMaxTrimmed());
+		result = calcLengthHistogramm(values, result);
+		
+		return result;
+	}
+	
+	protected StructureRecResult calcLengths(List<String> values, StructureRecResult result) {
+		boolean firstLoop = true;
+		for (String actValue : values) {
 			result.records++;
+			if (actValue != null ) {
+				if( actValue.length() > result.getLengthMax())
+					result.setLengthMax(actValue.length());
+				if( actValue.length() <= result.getLengthMin() || firstLoop)
+					result.setLengthMin(actValue.length());
+				actValue = actValue.trim();
+				if( actValue.length() > result.getLengthMaxTrimmed())
+					result.setLengthMaxTrimmed(actValue.length());
+				if( actValue.length() <= result.getLengthMinTrimmed() || firstLoop)
+					result.setLengthMinTrimmed(actValue.length());
+				firstLoop = false;
+			}			
 		}
 		return result;
 	}
 	
+	protected StructureRecResult calcLengthHistogramm(List<String> values, StructureRecResult result) {
+		for (String actValue : values) {
+			if (actValue != null ) {
+				actValue = actValue.trim();
+				result.addValueLengthHistogram(actValue.length());
+			}			
+		}
+		return result;
+	}
 	
 }
